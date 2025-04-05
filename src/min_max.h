37883e81c4a9dbb3dcc000dd1dfdbf4664f9b151
068432 <jsd982412@gmail.com>
2025-04-04 20:45:23 -0600

    begin some implementations

--- src/min_max.h
+++ src/min_max.h
@@ -1,5 +1,10 @@
 #ifndef MIN_MAX_H
 #define MIN_MAX_H
 
+#include <float.h>
+
+int double_max_element(const double* const arr, const unsigned int len);
+
+int double_min_element(const double* const arr, const unsigned int len);
 
 #endif
